5262d8afd47946f7d5c40fe629cfea166908a925
Andrew <andrewbrown600@gmail.com>
2017-09-13 14:40:50 -0700

    update: change family to fontFamily etc for clarity

--- src/components/Font/index.js
+++ src/components/Font/index.js
@@ -9,8 +9,8 @@ import { modularScale } from 'polished';
 
 const Font = css`
   font-size: ${(props) => modularScale(props.size, props.scaleBase, props.scaleRatio)};
-  font-family: ${(props) => props.family};
-  font-weight: ${(props) => props.weight};
+  font-family: ${(props) => props.fontFamily};
+  font-weight: ${(props) => props.fontWeight};
   line-height: ${(props) => props.lineHeight};
   color: ${(props) => props.color};
   margin-bottom: ${(props) => props.marginBottom};
--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -10,11 +10,11 @@ import Font from '../Font';
 
 const Heading = styled.h1.attrs({
   size: (props) => props.size,
-  family: (props) => props.fontFamily || props.theme.headingsFontFamily || 'inherit',
+  fontFamily: (props) => props.fontFamily || props.theme.headingsFontFamily || 'inherit',
   scaleBase: (props) => props.scaleBase || props.theme.scaleBase || 1,
   scaleRatio: (props) => props.scaleRatio || props.theme.scaleRatio || 'perfectFourth',
   color: (props) => props.color || props.theme.headingsColor || '#4B4B4B',
-  weight: (props) => props.weight || props.theme.headingsFontWeight || 600,
+  fontWeight: (props) => props.fontWeight || props.theme.headingsFontWeight || 600,
   lineHeight: (props) => props.lineHeight || props.theme.headingsLineHeight || modularScale(1),
   marginBottom: (props) => props.theme.headingsMarginBottom || '1rem',
 })`
